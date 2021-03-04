388df9cfa7c7c8b97337dcab0277b518c6f235c6
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-03-04 15:51:43 -0300

    Tratamento de Exceções

--- src/main/java/com/gui/projetowebservice/services/UserService.java
+++ src/main/java/com/gui/projetowebservice/services/UserService.java
@@ -2,6 +2,7 @@ package com.gui.projetowebservice.services;
 
 import com.gui.projetowebservice.entities.User;
 import com.gui.projetowebservice.repositories.UserRepository;
+import com.gui.projetowebservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> user = userRepository.findById(id);
-        return  user.get();
+        return user.orElseThrow(()->new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
@@ -30,4 +31,18 @@ public class UserService {
     public void delete(Long id){
         userRepository.deleteById(id);
     }
+
+    public User update(Long id, User obj){
+        User entity = userRepository.getOne(id);
+        updateData(entity,obj);
+        return  userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
+
 }
