3b4ca1f2b173b19169fd0bc753e46fcdd7937941
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-03-04 11:30:22 -0300

    Associações manytomany

--- src/main/java/com/gui/projetowebservice/entities/Category.java
+++ src/main/java/com/gui/projetowebservice/entities/Category.java
@@ -1,5 +1,7 @@
 package com.gui.projetowebservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -15,7 +17,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @OneToMany
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
