a2aa17258e1a658c9c4587fba752acf791459b34
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-03-04 16:01:01 -0300

    Tratamento de Exceções

---
+++ src/main/java/com/gui/projetowebservice/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.gui.projetowebservice.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+
+}
