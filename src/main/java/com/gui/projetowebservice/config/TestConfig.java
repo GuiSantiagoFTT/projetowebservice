a01e5b18fd4438cb60d8c948a6b95069d2f4321d
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-03-04 13:21:27 -0300

    Implementação da classe Order Item

--- src/main/java/com/gui/projetowebservice/config/TestConfig.java
+++ src/main/java/com/gui/projetowebservice/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.gui.projetowebservice.config;
 
-import com.gui.projetowebservice.entities.Category;
-import com.gui.projetowebservice.entities.Order;
-import com.gui.projetowebservice.entities.Product;
-import com.gui.projetowebservice.entities.User;
+import com.gui.projetowebservice.entities.*;
 import com.gui.projetowebservice.entities.enums.OrderStatus;
-import com.gui.projetowebservice.repositories.CategoryRepository;
-import com.gui.projetowebservice.repositories.OrderRepository;
-import com.gui.projetowebservice.repositories.ProductRepository;
-import com.gui.projetowebservice.repositories.UserRepository;
+import com.gui.projetowebservice.repositories.*;
 import com.gui.projetowebservice.resources.UserResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
 
     @Override
@@ -68,5 +65,11 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
     }
 }
